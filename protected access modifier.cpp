0014aed257e19cc86c9904b2f745bed049fb2d79
Aadit-Prashar <prasharaadit@gmail.com>
2026-07-19 13:05:24 +0530

    C++  file

---
+++ protected access modifier.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Base{
+	protected:
+		int a;
+	public:
+		int b;
+	
+};
+class Derived : protected Base{
+	
+};
+int main(){
+	Base b;
+	Derived d;
+	cout<<d.a; //Will not work since a is protected in both base as well as derived class
+	
+	return 0;
+}
